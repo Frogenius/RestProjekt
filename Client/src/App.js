d5ddf7be66ef966d997a1a81a50c8572ee0baec7
Irina Frolova <61787727+Frogenius@users.noreply.github.com>
2021-06-10 18:07:37 +0200

    Add files via upload

---
+++ Client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Przepisy } from './components/Przepisy';
+
+
+
+
+import './custom.css'
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+        <Route exact path='/' component={Home} />
+        <Route path='/przepisy' component={Przepisy} />
+            <Route path='/fetch-data' component={FetchData} />
+           
+          
+      </Layout>
+    );
+  }
+}
